ee7b44eecf60f8fd502e16ead0767038642a00fa
Benito Cazares IV <Benny@benitos-mbp.attlocal.net>
2016-04-20 09:26:41 -0500

    changed front pic

--- app/controllers/api/drop_in_controller.rb
+++ app/controllers/api/drop_in_controller.rb
@@ -1,43 +1,6 @@
-class Api::DropInController < ApplicationController
+# class Api::DropInController < ApplicationController
+#
+#   before_action :authenticate_user!
+#
 
-  before_action :authenticate_user!
-
-    def index
-      search_params =  {
-        term: 'Crossfit',
-        limit: 16,
-        sort: 1,
-        radius_filter: 20000
-      }
-
-      if params[:post_code]
-        search_term = params[:post_code]
-        @yelp_response =  Yelp.client.search(search_term, search_params)
-      elsif params[:lat] && params[:lng]
-        @user_coordinates = {
-          latitude: params[:lat],
-          longitude: params[:lng]
-        }
-
-        @yelp_response =  Yelp.client.search_by_coordinates(@user_coordinates, search_params)
-
-      end
-
-      @array = @yelp_response.businesses
-      @location_results = Gmaps4rails.build_markers(@array) do |business, marker|
-          marker.lat business.location.coordinate.latitude
-          marker.lng business.location.coordinate.longitude
-          marker.infowindow render_to_string(:partial => "infowindow",
-                                             :locals => {
-                                             :business => business})
-
-      end
-
-
-
-      render( json: {gmaps_data: @location_results.as_json})
-
-    end
-
-  end
-end
+# end
--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,12 @@
-# require 'yelp'
-#
 # Yelp.client.configure do |config|
-#   config.consumer_key = ENV["consumer_key"]
-#   config.consumer_secret = ENV["consumer_secret"]
-#   config.token = ENV["token"]
-#   config.token_secret = ENV["token_secret"]
+#   config.consumer_key = ENV['YELP_CONSUMER_KEY']
+#   config.consumer_secret = ENV['YELP_CONSUMER_SECRET']
+#   config.token = ENV['YELP_TOKEN']
+#   config.token_secret = ENV['YELP_TOKEN_SECRET']
+# end
+# Yelp.client.configure do |config|
+#   config.consumer_key = ENV["DAsvjkLxMTfrb255bKvw2w"]
+#   config.consumer_secret = ENV["im-gr7baWCENcaOn-YN5RgpKj5k"]
+#   config.token = ENV["Mzjp6VyaLDS7ywZFrUJ6MV9tcWKW26j-"]
+#   config.token_secret = ENV["Q5akJFXnorD5t5twZkv9w6Y_hT8"]
 # end
